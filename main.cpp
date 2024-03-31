b03e4266b13e21cbfb6be1ea4441c8d5c6a7c026
Joseph Kwame Agyei <jagyei339@gmail.com>
2024-03-31 00:21:40 +0000

    working algo

--- main.cpp
+++ main.cpp
@@ -5,28 +5,28 @@ using namespace std;
 
 std::vector<int> pool;
 
-int target=9;
+uint64_t target=999;
 
 void div_n_con(int begin,int len){
-    int get_middle=(len/2);
-cout<<"no seg fault at this point"<<endl;
+    int get_middle=((len-begin)/2)+begin;
+
     if(target>pool.at(get_middle)){
 
         div_n_con(get_middle+1,len);
 
     } else if(target<pool.at(get_middle)){
         
-        div_n_con(len-(get_middle*2),get_middle);
+        div_n_con(begin,len-1);
 
     } else{
-        cout<<"found target "<<target<<" at "<<pool.at(get_middle)<<endl;
+        cout<<"found target "<<target<<" at index "<<get_middle<<" which results "<<pool.at(get_middle)<<endl;
     };
 
 };
 
 
 int main(){
-    for(int i=0;i<10;i++){
+    for(uint64_t i=0;i<1000;i++){
         pool.push_back(i);
     };
 
@@ -34,10 +34,6 @@ int main(){
 
     *len=pool.size();
 
-cout<<"length is "<<*len<<endl;
-for(int i:pool)
-    cout<<i<<endl;
-    cout<<"mid  - "<<*len/2<<endl;
     div_n_con(0,*len);
 
     delete len;
